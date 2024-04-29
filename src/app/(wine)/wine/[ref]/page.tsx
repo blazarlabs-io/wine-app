f0eb6f4a1fcbe63886da5b078e706cb6f746a2e6
technoshaman <fitosegrera@gmail.com>
2024-04-28 20:26:44 -0500

    Update page.tsx

--- src/app/(wine)/wine/[ref]/page.tsx
+++ src/app/(wine)/wine/[ref]/page.tsx
@@ -12,13 +12,16 @@ import {
 } from "@/utils/firestore";
 import { DocumentData } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function WineExplorer({ params }: { params: { ref: string } }) {
   const [generalInfo, setGeneralInfo] =
     useState<WineryGeneralInfoInterface | null>(null);
+  const router = useRouter();
   const [euLabel, setEuLabel] = useState<DocumentData | null>(null);
 
   useEffect(() => {
+    router.refresh();
     getWineByRefNumber(params.ref, (label: EuLabelInterface | null) => {
       setEuLabel(label);
     });
